d8b808a537075415ff38d5bb7307d15da1d06492
Chris Smith <smitty1e@gmail.com>
2021-05-27 22:12:14 -0400

    Got the service working, first clean call

--- tests/test_fossil_service.py
+++ tests/test_fossil_service.py
@@ -1,10 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
 from subprocess import run
 import pytest
 
 @pytest.fixture
 def status():
-    return ['systemctl','--user','status','-l','fossil.socket']
+    return ['systemctl','--user','status','-l','fossil.service']
 
-def test_fossile_service_running(status):
+@pytest.mark.skip
+def test_fossil_service_running(status):
     x = run(status)
     assert x.returncode==0
+
+def test_fossil_version():
+    r = requests.get('http://127.0.0.1:9000/version?verbose')
+    soup = BeautifulSoup(r.text,'html.parser')
+    v = soup.find_all('div',class_='content')
+    print(v)
