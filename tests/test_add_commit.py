e86db0b153e2521e26ae601abcfb5d98eb09282e
Chris Smith <smitty1e@gmail.com>
2021-05-31 19:18:50 -0400

    First working add test

--- tests/test_add_commit.py
+++ tests/test_add_commit.py
@@ -1,51 +1,116 @@
-from dataclasses import dataclass
+from os import chdir
 from pathlib import Path
-from bs4 import BeautifulSoup as Bs
-import requests
+from shutil import copy2 as copy
+from subprocess import run
+
 import pytest
 
-@dataclass
-class Requ:
-    requestId: str
-    command:   str
-    authToken: str
-    payload:   str
-    indent:    int
-    jsonp:     str
+@pytest.fixture
+def fossil():
+    return '/home/osboxes/src/fossil-snapshot-20210429/fossil'
 
-
-@dataclass
-class Resp:
-    fossil:    str
-    requestId: str
-    resultCode: str
-    resultText: str
-    payload: str
-    timestamp: str
-    payloadVersion: int
-    command: str
-    apiVersion: int
-    warnings: str
-    g: str
-    procTimeMs: int
 
 @pytest.fixture
-def url():
-    return 'http://127.0.0.1:9000/json/'
+def repo_path():
+    return '/home/osboxes/proj/pyphlogiston/pyphlogiston/repo'
+
+@pytest.fixture
+def repo_name():
+    return 'phlogiston.fossil'
 
 @pytest.fixture
 def data_path():
     return '/home/osboxes/proj/pyphlogiston/pyphlogiston/data'
 
 @pytest.fixture
+def setup_script(tmp_path, fossil, repo_path, repo_name, data_path):
+    '''
+    1. set up a data directory
+    2. set up a data/checkout directory
+    3. set up a data/repo directory
+    4. initialize data/repo/repo.fossil
+    5. checkout data/repo/repo.fossil to data/checkout
+
+    return the staging path
+    '''
+    #TMP_PATH would be the install directory
+    # 1, 2, 3:
+    b = Path(str(tmp_path) + 'data')
+    b.mkdir()
+    for d in ['stage','repo']:
+        c = Path(str(b) + d)
+        c.mkdir()
+
+    # 4.
+    r = Path(str(tmp_path) + '//data//repo' )
+    print(f'{r=}')
+    r.mkdir(parents=True)
+    chdir(str(r))
+    init = [fossil,'init', 'phologiston.fossil']
+    out = run(init, capture_output=True)
+    assert out.returncode == 0
+
+    # 5.
+    s = Path(str(tmp_path) + '//data//stage' )
+    print(f'{s=}')
+    co =  [fossil,'open', f'{str(r)}/phologiston.fossil', '--workdir', str(s)]
+    out = run(co, capture_output=True)
+    assert out.returncode == 0
+
+    return s
+
+
+
+
+@pytest.fixture
+def test_repo(fossil,tmp_path,repo_name):
+    f = []
+    f.append(fossil)
+    test_path = f'{tmp_path}/{repo_name}'
+    f.append('init')
+    f.append(test_path)
+    #print(f'{" ".join([f for f in fossil])}')
+    out = run(f, capture_output=True)
+    assert out.returncode == 0
+    return test_path
+
+@pytest.fixture
 def add_files0(data_path):
     p = Path(data_path)
     return [str(f) for f in p.glob('./*')]
 
-def test_fossil_add(url,add_files0):
-    u=f'{url}add'
-    print(f'{u=}')
-    r = requests.post(u)
-    soup = Bs(r.text,'html.parser')
-    #v = soup.find_all('div',class_='content')
-    print(soup)
+def test_fossil_version(fossil):
+    f = []
+    f.append(fossil)
+    f.append('version')
+    out = run(f, capture_output=True)
+    assert out.returncode == 0
+
+def test_repo_info(fossil,test_repo):
+    f = []
+    f.append(fossil)
+    f.append('info')
+    f.append(test_repo)
+    #fossil.append('verbose')
+    out = run(f, capture_output=True)
+    assert out.returncode == 0
+    #print()
+    #print(out.stdout.decode('utf-8'))
+
+def test_add(setup_script, fossil, add_files0):
+    '''
+    6. stick files in data/checkout
+    7. do the rest of the fossil thing
+    '''
+    chdir(setup_script)
+    for a in add_files0:
+        copy(a, setup_script)
+
+    f = []
+    f.append(fossil)
+    f.append('add')
+    f.append(f'{setup_script}/*')
+    out = run(f, capture_output=True)
+    assert out.returncode == 0
+    #print()
+    #print(out.stdout.decode('utf-8'))
